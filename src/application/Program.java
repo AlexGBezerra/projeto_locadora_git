fcc30c2924197857bf2c50a4fa03d128b15cd26a
Alex Bezerra <alexgbezerra@hotmail.com>
2023-02-10 17:40:50 -0300

    Classe Calculo Aluguel

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,56 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		int kminicial, kmfinal, somakm, kmtotal, kmincluso, qdias;
+
+		double diaria, somadiaria, precoKm;
+
+		System.out.println("Digite o valor da diaria: ");
+		diaria = sc.nextDouble();
+
+		System.out.println("Digite quantidade de  dias: ");
+		qdias = sc.nextInt();
+
+		System.out.println("Digite km inicia: ");
+		kminicial = sc.nextInt();
+
+		System.out.println("Digite o km incluso: ");
+		kmincluso = sc.nextInt();
+
+		System.out.println("Digite km final: ");
+		kmfinal = sc.nextInt();
+
+		System.out.println("Digite o valor do km excedido: ");
+		precoKm = sc.nextDouble();
+
+		somakm = kmfinal - kminicial;
+
+		somadiaria = qdias * diaria;
+
+		kmtotal = kmincluso * qdias;
+
+		if (somakm <= kmtotal) {
+
+			somadiaria = qdias * diaria;
+
+		} else if (somakm >= kmtotal) {
+
+			somadiaria += (somakm - kmincluso * qdias) * precoKm;
+
+		}
+
+		System.out.printf("Valor total a pagar: R$ %.2f%n", somadiaria);
+
+		sc.close();
 	}
-
 }
